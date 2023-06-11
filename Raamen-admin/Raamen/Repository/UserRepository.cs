69600812b3369e7cc09eba27e37c67db44341bf5
kelvianabbie <136244426+kelvianabbie@users.noreply.github.com>
2023-06-11 23:16:00 +0700

    Initial commit

---
+++ Raamen-admin/Raamen/Repository/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using Raamen.Factory;
+using Raamen.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Raamen.Repository
+{
+    public class UserRepository
+    {
+        public static RaamenEntities db = new RaamenEntities();
+
+        public static User getUserFromEmailAndPassword(string email, string password)
+        {
+            return db.Users.Where(user => (user.Email == email || user.UserName == email) && user.Password == password).FirstOrDefault();
+        }
+
+        public static User createUser(string username, string email, string gender, string password)
+        {
+            return UserFactory.createUser(username, email, gender, password);
+        }
+
+        public static void addUser(User u)
+        {
+            db.Users.Add(u);
+            db.SaveChanges();
+        }
+        public static User getUserByID(int id)
+        {
+            return db.Users.Where(user => user.Id == id).FirstOrDefault();
+        }
+
+        public static bool duplicateAccount(string email, string username)
+        {
+            return (db.Users.Where(user => user.Email == email || user.UserName == username).FirstOrDefault() == null);
+        }
+
+        public static List<User> getAllCustomer()
+        {
+            return db.Users.Where(user => user.Id == 3).ToList();
+        }
+
+        public static List<User> getAllStaff()
+        {
+            return db.Users.Where(user => user.Id == 2).ToList();
+        }
+
+    }
+}
